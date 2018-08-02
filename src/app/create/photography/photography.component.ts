b6e05181bbc83e1031e35c5dc17d494a185718d1
chiangs <stephen.e.chiang@gmail.com>
2018-08-02 06:53:01 +0200

    Removed text in photography

--- src/app/create/photography/photography.component.ts
+++ src/app/create/photography/photography.component.ts
@@ -13,10 +13,6 @@ import { ThemeSelectService } from '../../services/theme-select.service';
       <h1>{{ title }}</h1>
       <h2>{{ title2 }}</h2>
     </div>
-    <div class="cta">
-      <p>{{ cta }} <b>{{ ctaTech }}</b></p>
-      <p>{{ cta2 }}</p>
-    </div>
   </div>
   <app-gallery [gallery]="gallery"></app-gallery>
   `,
@@ -41,8 +37,6 @@ export class PhotographyComponent implements OnInit, OnDestroy {
     this.title = `A way to find inspiration and lessons in design.`;
     this.title2 = `A creative outlet to develop observational and detail-oriented aspects of problem-solving skills.`;
     this.cta = `This gallery is built with`;
-    this.ctaTech = `CSS Grid`;
-    this.cta2 = `Play around with the display size and check out the responsiveness as it shuffles the photos to reduce white space!`;
     this.themeSub = this.themeSvc
       .getTheme()
       .subscribe(theme => (this.theme = theme));
